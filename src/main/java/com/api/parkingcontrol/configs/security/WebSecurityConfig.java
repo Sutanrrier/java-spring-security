78d8261345db1adf4dfdd2502fde187ea8615781
Sutanrrier <pabloryanbr0@gmail.com>
2022-10-03 11:22:55 -0300

    Exemplo Sring Security by Michelli Brito

---
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.api.parkingcontrol.configs.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@Configuration - Spring Security antes da versão 5.7
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+			.and().authorizeHttpRequests()
+			.antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
+			.antMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
+			.antMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
+			.anyRequest().authenticated()
+			.and().csrf().disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
